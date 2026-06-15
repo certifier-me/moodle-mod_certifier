32a37f5d3a4981cc09cbfd5cb6d576e0c29bcbb7
Marcin Kwiek <marcin@certifier.io>
2026-06-15 11:10:51 +0200

    Add Moodle Plugin GH Actions workflow

--- db/upgrade.php
+++ db/upgrade.php
@@ -22,8 +22,6 @@
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Upgrade the Certifier activity module database schema.
  *
